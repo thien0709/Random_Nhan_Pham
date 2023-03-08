3e029a71d2c9992383f4865efa3fdce06e14dcfd
thien0709 <thien070904@gmail.com>
2023-03-08 19:36:08 +0700

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector('button');
+const input = document.querySelector('input');
+const imgs = document.querySelectorAll('.pic img');
+function getRandom(){
+    return random = Math.floor(Math.random()*6)+1;
+}
+function getRandoms(){
+    
+}
+btn.onclick = function(e){
+    //Lay du lieu tu input value
+    const value = input.value;
+    //Random 
+    const arrRandom=[]
+   
+
+    //Xu li diem
+
+}
+// console.log(btn,imgs,);
+// btn.addEventListener("click", function(){
+//     if (this.classList.contains('changed')) {
+
+//     }
+// }
+// )
